92066e253a772cdc9a94908f27cd1522fbe4db19
ihgezly <ihgezly0pls@gmail.com>
2026-08-21 03:20:32 +0000

    رسالة توضح التغيير

--- apps/api/src/api/admin/custom/route.ts
+++
@@ -1,8 +0,0 @@
-import { MedusaRequest, MedusaResponse } from "@medusajs/framework/http";
-
-export async function GET(
-  _req: MedusaRequest,
-  res: MedusaResponse
-) {
-  res.sendStatus(200);
-}
